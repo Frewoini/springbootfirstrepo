109ad2b3ab55c81049e1414de0fa0af4c00774ab
Frewoini G.Michael <103339531+Frewoini@users.noreply.github.com>
2022-06-29 15:34:32 -0700

    Add files via upload

---
+++ src/main/java/com/firstrepo/firstrepo/FirstrepoApplication.java
@@ -0,0 +1,13 @@
+package com.firstrepo.firstrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstrepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firstrepo/firstrepo/FirstrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstrepo.firstrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
